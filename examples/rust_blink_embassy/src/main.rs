18ed4f667adfdb83bd9f881e91ac9d1b2eea2221
Brandon Saint-John <saint-john@lbl.gov>
2026-05-20 22:22:49 -0700

    feat: generate blinky example

---
+++ examples/rust_blink_embassy/src/main.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+use cortex_m::asm;
+use cortex_m_rt::entry;
+use panic_halt as _;
+
+use embassy_cxd56::gpio::{Level, Output};
+
+// ~0.5 s at 153.6 MHz
+const DELAY_CYCLES: u32 = 76_800_000;
+
+#[entry]
+fn main() -> ! {
+    let p = embassy_cxd56::init();
+    let mut led = Output::new(p.PIN97, Level::Low);
+
+    loop {
+        led.set_high();
+        asm::delay(DELAY_CYCLES);
+        led.set_low();
+        asm::delay(DELAY_CYCLES);
+    }
+}
