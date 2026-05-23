9a0179df5137f0d65f3a85ccd223c01cabad46db
Brandon Saint-John <saint-john@lbl.gov>
2026-05-23 08:08:28 -0700

    feat(example): update hal example to use clock config and delay

--- examples/rust_blink_hal/src/main.rs
+++ examples/rust_blink_hal/src/main.rs
@@ -6,6 +6,7 @@ use cortex_m_rt::entry;
 use embedded_hal::delay::DelayNs;
 use panic_halt as _;
 
+use cxd56_hal::clocks::{Config, RccExt};
 use cxd56_hal::delay::Delay;
 use cxd56_hal::gpio::{gpio0, Level};
 use cxd56_hal::pac;
@@ -15,9 +16,12 @@ fn main() -> ! {
     let pac = pac::Peripherals::take().unwrap();
     let core = Peripherals::take().unwrap();
 
+    let crg = pac.crg.constrain(Config::default());
+    let clocks = crg.freeze();
+
     let pins = gpio0::Parts::new(pac.gpio0);
     let mut led = pins.pin97.into_output(Level::Low);
-    let mut delay = Delay::new(core.SYST);
+    let mut delay = Delay::new(core.SYST, &clocks);
 
     loop {
         led.set_high();
