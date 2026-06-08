ee29a75735138baef841656c5ff1fabfccefcd57
Brandon Saint-John <branstj@gmail.com>
2026-06-07 23:32:43 -0700

    feat: update with pins and peripherals

--- embassy-cxd56/src/lib.rs
+++ embassy-cxd56/src/lib.rs
@@ -5,8 +5,37 @@ pub use cxd56_pac_chiptool as pac;
 pub mod gpio;
 
 embassy_hal_internal::peripherals! {
-    GP_I2S1_BCK,
+    UART1,
+    UART2,
+    I2S0,
+    SPI5,
+    I2C0,
+    AIN4,
+    AIN5,
+
+    P1e_00,
+    P1j_00,
+    P1j_01,
+    P1n_00,
+    P1n_01,
+    P1n_02,
+    P1n_03,
+    P1p_00,
+    P1p_01,
+    P1p_02,
+    P1p_03,
+    P1q_00,
+    P1q_01,
+    P1v_00,
+    P1v_01,
+    P1v_02,
+    P1v_03,
+    P1w_00,
+    P1w_01,
+    P1w_02,
+    P1w_03,
 }
+
 
 pub fn init() -> Peripherals {
     Peripherals::take()
--- examples/rust_blink_embassy/src/main.rs
+++ examples/rust_blink_embassy/src/main.rs
@@ -5,7 +5,7 @@ use cortex_m::asm;
 use cortex_m_rt::entry;
 use panic_halt as _;
 
-use embassy_cxd56::gpio::{Level, Output};
+use embassy_cxd56::gpio::{Level, Output, OutputDrive};
 
 // ~0.5 s at 153.6 MHz
 const DELAY_CYCLES: u32 = 76_800_000;
@@ -13,7 +13,7 @@ const DELAY_CYCLES: u32 = 76_800_000;
 #[entry]
 fn main() -> ! {
     let p = embassy_cxd56::init();
-    let mut led = Output::new(p.GP_I2S1_BCK, Level::Low);
+    let mut led = Output::new(p.P1w_00, Level::Low, OutputDrive::Standard);
 
     loop {
         led.set_high();
